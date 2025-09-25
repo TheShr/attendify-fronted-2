7f682fbe129fcad9bc2b8c7aa3358ce0229fbbea
TheShr <anujsharma214692@gmail.com>
2025-09-25 12:29:17 +0530

    fix: proxy API requests to Render backend

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
   async rewrites() {
     return [
       {
+        source: "/api/classes",
+        destination: "https://attendify-wnl8.onrender.com/api/teacher/courses",
+      },
+      {
         source: "/api/:path*",
-        destination: "https://attendify-wnl8.onrender.com/api/:path*", // Flask backend
+        destination: "https://attendify-wnl8.onrender.com/api/:path*",
       },
     ];
   },
