fb5dd90955ed02fa794772e9e4448918038d65d4
TheShr <anujsharma214692@gmail.com>
2025-09-25 12:03:31 +0530

    fix firebase hosting config and cleanup out dir

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Don’t use output: 'export' since you need API rewrites
   reactStrictMode: true,
 
+  // Keep using SSR (do NOT use output: 'export')
   async rewrites() {
     return [
       {
